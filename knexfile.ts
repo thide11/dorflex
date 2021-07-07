992649de5d41537208910df2def6410e9c9e3b65
THIAGO VIANA AVELAR DE SOUZA <thide2001@gmail.com>
2021-07-07 17:55:34 -0300

    Adicionado suporte a configuração de produção

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,7 @@
 import getEnvOrReturnError from "./src/infrastructure/utils/get-env-or-return-error";
 
-export default {
-  development: {
+function getDefaultConfig() : any {
+  return {
     client: "pg",
     version: "13.3",
     connection: {
@@ -14,22 +14,16 @@ export default {
     migrations: {
       directory: __dirname + '/src/infrastructure/repositories/knex/migrations'
     },
-  },
+  }
+}
+
+export default {
   test: {
-    client: "pg",
-    version: "13.3",
-    connection: {
-      host: getEnvOrReturnError("POSTGRES_HOST"),
-      port: 5432,
-      user: getEnvOrReturnError("POSTGRES_USER"),
-      password: getEnvOrReturnError("POSTGRES_PASSWORD"),
-      database: getEnvOrReturnError("POSTGRES_DB"),
-    },
+    ...getDefaultConfig(),
     seeds: {
       directory: __dirname + '/test/repositories/knex/seeds'
     },
-    migrations: {
-      directory: __dirname + '/src/infrastructure/repositories/knex/migrations'
-    },
   },
+  development: getDefaultConfig(),
+  production: getDefaultConfig(),
 }
